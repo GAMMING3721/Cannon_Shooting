ef4e64fe32f169c8c87ffee2dfd1d48722384dcc
GAMMING3721 <79403216+GAMMING3721@users.noreply.github.com>
2021-02-21 17:25:26 +0530

    Add files via upload

---
+++ Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(this.gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = -transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Player"))
+        {
+            Health_Player Player = coll.gameObject.GetComponent<Health_Player>();
+            Player.DamagePlayer(10);
+            Destroy(this.gameObject);
+        }
+        if (coll.gameObject.tag.Equals("Default"))
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject, 5f);
+        }
+    }
+}
---
+++ cannonscript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonscript : MonoBehaviour
+{
+    public Transform attackpos,bulletspawnpoint;
+    public LayerMask WhatisPlayer;
+    public float attackrange;
+    public float timepershoot = 3;
+    float time, i;
+    public GameObject Bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D[] whatisPlayer = Physics2D.OverlapCircleAll(attackpos.position, attackrange, WhatisPlayer);
+        if (whatisPlayer.Length != 0)
+        {
+            if (i == 0)
+            {
+                time = Time.time;
+                i++;
+            }
+            if (Time.time >= time)
+            {
+                Shoot();
+                time += timepershoot;
+            }
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(Bullet, bulletspawnpoint.position, bulletspawnpoint.rotation);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackpos.position, attackrange);
+    }
+
+}
